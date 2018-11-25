9861aa5744193d03df9c2b424cea2092af00ea8b
arnob08 <rashed.arnob@gmail.com>
2018-11-25 02:48:42 -0500

    3rd versoin of homework

--- src/main/Toyota.java
+++ src/main/Toyota.java
@@ -2,4 +2,26 @@ package main;
 
 public class Toyota {
 
-}
+
+    int price;
+
+    Toyota camry = new Toyota(30);
+    Toyota corolla = new Toyota(30);
+    Toyota rav = new Toyota(40);
+    Toyota highLander = new Toyota(60);
+    Toyota sienna = new Toyota(70);
+
+
+    public Toyota(int price) {
+        this.price = price;
+
+
+    }
+
+    public static void main(String[] args) {
+
+            System.out.println("price");
+        }
+    }
+
+
