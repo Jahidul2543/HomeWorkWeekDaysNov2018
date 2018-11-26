c41a3f8ed4f4f02c82ae4cc21191d641aac9570b
Marzana Naz <nazmarzana@gmail.com>
2018-11-26 13:13:09 -0500

    class 11

---
+++ src/main/Train.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Building {
+    int age = 1;
+    String color = "white";
+    int floors = 2;
+    boolean basement = true;
+    int squareFootage = 3000;
+
+    public void 
+
+}
