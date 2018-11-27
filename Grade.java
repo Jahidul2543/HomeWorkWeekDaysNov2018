e66ebaaf1815e81ed52c45f4fe1427d3685c46f2
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-11-26 23:45:25 -0500

    java practice4

---
+++ Grade.java
@@ -0,0 +1,35 @@
+package main;
+
+public class Grade {
+
+    public static void main (String [] args){
+
+
+        int females = 15;
+        int males = 10;
+        int kids = 20;
+
+
+        System.out.println(females + kids);
+
+        System.out.println(males + kids);
+
+        System.out.println( males - females);
+
+        System.out.println ( "these are the number of students in each grade");
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
