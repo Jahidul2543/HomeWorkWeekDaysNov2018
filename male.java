42280a3bc102611102f6e7e0d83097357451e282
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-11-26 22:55:03 -0500

    java practice2

---
+++ male.java
@@ -0,0 +1,24 @@
+package main;
+
+public class male {
+
+    public static void main (String [] args){
+
+
+        String name = "tom";
+        int age = 7;
+        byte grade = 2;
+
+
+        System.out.println( name + " is " + age + " in " + grade );
+
+        age = (int) (age + 1);
+
+        System.out.println(name + " will turn " + age + " years old " );
+
+
+
+
+
+    }
+}
