1882899ad2d6a2708336e7abc1f1f1ec163fd603
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-11-27 01:23:12 -0500

    java practice7

---
+++ mathOperators.java
@@ -0,0 +1,18 @@
+package main;
+
+public class mathOperators {
+
+
+    public static void main(String[] agrs) {
+
+
+        int score = 40;
+
+        score = 400 + (score + 10);
+
+        System.out.println(score);
+
+    }
+}
+
+
