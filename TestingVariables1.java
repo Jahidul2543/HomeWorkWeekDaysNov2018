55a6ab97237fc3be702708a17791ce92e3c5d717
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-11-27 01:05:03 -0500

    java practice6

---
+++ TestingVariables1.java
@@ -0,0 +1,28 @@
+package main;
+
+public class TestingVariables1 {
+
+    public static void main (String[]args){
+
+        String name;
+        name = "Bob";
+
+        int touchdown;
+        touchdown = 6;
+
+        byte score;
+        score = 10;
+
+        double points;
+        points = 2.3;
+
+
+        System.out.println(name + touchdown + score + points);
+
+
+
+
+
+    }
+
+}
