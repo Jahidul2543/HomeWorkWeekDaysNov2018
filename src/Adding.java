6f5cf7c8cad6561f726952886ebffd4a982f98cf
Ziyad90 <Hussain.ziyad90@gmail.com>
2018-11-28 13:03:41 -0500

    HomeworkJAva

---
+++ src/Adding.java
@@ -0,0 +1,13 @@
+public class Adding {
+
+    int x;
+    int y;
+    int z;
+
+    int age x=9;
+    int y=5;
+    int =(x+y);
+
+
+}
+
---
+++ src/com/company/Learn_Java.java
@@ -0,0 +1,9 @@
+
+
+class FistCode{
+    public static void main(String[] args) {
+        System.out.println("Hello World,I am Ziyad");
+    }
+
+
+}
