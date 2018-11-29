789e0290b5e3b3b9d1534f94363755980a1c5021
Md  hasan <mdhasan@Mds-MacBook-Pro-2.local>
2018-11-29 08:21:11 -0500

    second commit

--- 3784ny_homework_java/src/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car extends Vehicle {
-
-    public static void main(String args[]) {
-
-        Vehicle a = new Car();
-        boolean result =  a instanceof Car;
-        System.out.println( result );
-    }
-}
