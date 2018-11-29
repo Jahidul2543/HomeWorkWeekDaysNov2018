28286dc3830acebfe8b46ef3214a7aaa6b5e2129
Ziyad90 <Hussain.ziyad90@gmail.com>
2018-11-29 11:55:26 -0500

    Commit

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+
+        Person person1= new Person();
+
+        person1.name= "Ziyad";
+        person1.age= 27;
+
+
+
+
+        }
+    }
+
+
