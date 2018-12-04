ddb54c72b447889304dbeebcc15327675f8deb81
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-12-04 03:49:37 -0500

    java practice

---
+++ StringLength.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class StringLength {
+
+    public static void main (String[]args) {
+
+
+        String a = "HotCoffee";
+
+        //"HotCoffee".length();
+
+        System.out.println(a.length());
+
+        System.out.println("The length of " + a + " is : " + a.length());
+
+
+    }
+}
