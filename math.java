8432c771d0244b46b26bd0b29136c2c46c813a16
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-12-04 03:40:02 -0500

    java practice

---
+++ math.java
@@ -0,0 +1,24 @@
+package abstraction;
+
+public class math {
+
+    public static void main(String[]args){
+
+        int girls = 11;
+
+        int boys = 5;
+
+        int people = girls % boys;
+
+
+        System.out.print ( people);
+
+    }
+}
+
+// these are simple math symbols used to calculate variables.
+//  the + sign means add.
+//  the - sign means subtract.
+//  the * sign means to multiply.
+//  the / sign means to divide.
+//  the % sign is to find the left over from the division.
