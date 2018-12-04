204dab8463800edbb5fd098c71488286c7c2cb95
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-12-04 03:37:15 -0500

    java practice

---
+++ incrementOperators.java
@@ -0,0 +1,25 @@
+package main;
+
+public class incrementOperators {
+
+    public static void main(String[]args){
+       int girls = 10;
+        int boys = 5;
+
+       boys +=8;                      //    This can be written to add 8 more to the variable which is 5.
+
+                                       //     This is PRE-INCREMENT
+        System.out.println (++girls);  //     When you add ++ before the variable it adds 1 to the value of the variable.
+
+        System.out.println (girls++);
+        System.out.println (girls);    //      This is POST-INCREMENT
+                                       //      When you put ++ after the variable it doesnt add 1 until you make
+                                       //      add another out put.
+
+        System.out.println (--boys);   //      You can use -- for subtraction.
+
+
+
+
+}
+}
