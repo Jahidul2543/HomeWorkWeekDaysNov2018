cb1e7a62db0771934d5225da33ef9205087d78e7
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-12-04 03:22:42 -0500

    java practice

---
+++ andOr.java
@@ -0,0 +1,27 @@
+package main;
+
+public class andOr {
+    public static void main (String[]args) {
+
+        int boy, girl;
+        boy = 20;
+        girl = 18;
+
+
+        if (boy > 19 && girl <20){                      //          the && sign means "and"both has to be true
+                                                        //          the || sign means "or" which means one of
+            System.out.println ("correct");             //          the statement has to be true.
+
+        } else {
+            System.out.println ( "incorrect");
+
+
+
+
+
+
+
+        }
+    }
+}
+
---
+++ ifElse.java
@@ -0,0 +1,21 @@
+package HomeWorkWeekDaysNov2018;
+
+public class ifElse {
+
+    public static void main (String[]args){
+
+        int test = 4;
+
+         if (test > 4 && test > 4){
+             System.out.println ( "yes");     //  If it is true "yes" will print
+                                              //  The && sign means "and". Both has to be true for the if
+                                              //  If statement to show.
+
+                                              //  The || sign means "or". If one statement is true the if
+                                              //  statement will show.
+         }else{
+             System.out.println("no");        //  If it is false this "no " will print.
+
+        }
+    }
+}
