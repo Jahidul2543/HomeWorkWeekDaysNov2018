44d3b2248ecdf6c70e60163e2e9d2f2e22b4feba
sesire <45247250+sesire@users.noreply.github.com>
2018-12-04 04:51:24 -0500

    arrays practise

---
+++ arrays.java
@@ -0,0 +1,47 @@
+public class arrays {
+
+
+    public static void main(String[] args) {
+        int anArray[];
+        anArray = new int[10];
+
+        anArray[0] = 10;
+        anArray[1] = 20;
+        anArray[2] = 30;
+        anArray[3] = 40;
+        anArray[4] = 50;
+        anArray[5] = 60;
+        anArray[6] = 70;
+        anArray[7] = 80;
+        anArray[8] = 90;
+        anArray[9] = 100;
+
+
+        System.out.println("Print the value of "+ anArray[0]);
+        System.out.println("Print the value of "+anArray[1]);
+        System.out.println("Print the value of "+anArray[2]);
+
+
+        String myArray[];
+
+        myArray=new String[3];
+
+        myArray[0]= "sesire";
+        System.out.println(myArray[0]);
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
