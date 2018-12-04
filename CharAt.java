8662f5465609ddb1f8c81c2cc7afef561a1d13a3
Jeny Chowdhury <jenychowdhury@Jenys-MacBook-Pro.local>
2018-12-04 03:48:08 -0500

    java practice

---
+++ CharAt.java
@@ -0,0 +1,24 @@
+package strings;
+
+
+public class CharAt {
+
+    public static void main(String[]args){
+
+        String x = "noteBook";
+
+        System.out.println(x.charAt(4));
+
+        System.out.println(x.charAt(8));
+
+        System.out.println(x.charAt(2));
+
+        System.out.println(x.charAt(6));
+
+
+
+
+
+    }
+
+}
