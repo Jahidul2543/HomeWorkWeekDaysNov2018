aafeeefaa4f35f7b0443c03e79998334b58f340e
gravel93 <agaevruslan223@gmail.com>
2018-12-10 05:46:20 -0500

    Monday commitment

--- src/Basta.java
+++ src/Basta.java
@@ -1,6 +1,6 @@
-public class Main {
+public class Basta {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Basta!");
     }
 }
