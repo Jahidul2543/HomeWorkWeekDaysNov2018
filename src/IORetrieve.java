04ad68dde45cf91520bac62ac9b14c77fb4bc54b
gravel93 <agaevruslan223@gmail.com>
2018-12-11 02:25:28 -0500

    monday

---
+++ src/IORetrieve.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class IORetrieve {
+
+
+
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fl = new FileInputStream("Specify the Path to file here ");
+
+            int i;
+
+            while((i=fl.read())!=-1){
+
+                System.out.print(((char)i));
+            }
+            System.out.println();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
